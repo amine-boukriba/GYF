9d442d4d4ffc1bdd7948f40db027e2245eb3976f
mohamed amine boukriba <boukriba.mohamed.amine@gmail.com>
2022-02-11 21:03:56 +0100

    structure_project

--- src/entities/NewClass.java
+++ src/entities/NewClass.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gyf;
+package entities;
 
 /**
  *
  * @author boukr
  */
-public class GYF {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class NewClass {
     
 }
