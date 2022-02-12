4b7b42e9a20bd37146efed31e5c8c5239c3255e2
Akram-Zran <akram.zran@esprit.tn>
2022-02-12 11:17:44 +0100

    structure + connexion base de donnees

---
+++ src/test/Main.java
@@ -0,0 +1,14 @@
+
+package test;
+
+import utils.MyDB;
+
+
+public class Main {
+     
+         public static void main(String[] args){
+                MyDB.getInstance();
+    }
+    
+    }
+   
