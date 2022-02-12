d93e3514ae22cbf43f28b70272cfa0cc5cf59bf6
Akram-Zran <akram.zran@esprit.tn>
2022-02-12 21:02:19 +0100

    crud des classes monument et espace culturel

---
+++ src/services/IService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import java.util.List;
+
+/**
+ *
+ * @author akram
+ */
+public interface IService <T> {
+    
+    public void ajouter(T t);
+    public void modifier(T t);
+    public void supprimer(T t);
+    public List <T> afficher();
+    
+    
+}
