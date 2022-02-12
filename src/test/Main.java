d1650b644ce06efa6921825de6880dd44cfe6d7b
mohamed amine boukriba <boukriba.mohamed.amine@gmail.com>
2022-02-12 11:19:03 +0100

    changes

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,12 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package utils;
+package test;
+
+import utils.Mydb;
 
 /**
  *
  * @author boukr
  */
-public class NewClass {
+public class Main {
+    public static void main(String[] args) {
+        Mydb.getInstance();
+    }
     
 }
