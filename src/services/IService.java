ad8a52c2578d2bf9396708a5c3fe5bc78a33ec0a
OmarBouchniba3A12 <omar.bouchniba@esprit.tn>
2022-02-13 22:00:02 +0100

    CRUD hotel_restaurant

--- src/services/IService.java
+++ src/services/IService.java
@@ -13,8 +13,8 @@ import java.util.List;
  */
 public interface IService<T> {
     
-    public void ajout(T t);
-    public void modifier(T t);
-    public void supprime(int id);
-    public List<T> affiche();
+    public void create(T t);
+    public void modify(T t);
+    public void delete(int id);
+    public List<T> afficher();
 }
