83a61a23646c70738945d6cd24fc0515c4f4d34f
akram317 <akram.zran@esprit.tn>
2022-03-06 19:30:39 +0100

    design + 2 API

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
           try {
-            Parent root = FXMLLoader.load(getClass().getResource("EspaceAdmin.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("Template.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
